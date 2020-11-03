8e28b797cb5cfd20a9ffd90ed5383b9258083515
RezMatic <48518947+RezMatic@users.noreply.github.com>
2020-11-03 12:49:54 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,4 @@
+(function($){skel.breakpoints({xlarge:'(max-width: 1680px)',large:'(max-width: 1280px)',medium:'(max-width: 980px)',small:'(max-width: 736px)',xsmall:'(max-width: 480px)'});$(function(){var $window=$(window),$body=$('body'),$header=$('#header'),$banner=$('#banner');var $height=$('#header').height();$body.addClass('is-loading');$window.on('load',function(){window.setTimeout(function(){$body.removeClass('is-loading')},100)});$('form').placeholder();skel.on('+medium -medium',function(){$.prioritize('.important\\28 medium\\29',skel.breakpoint('medium').active)});if($banner.length>0){if(skel.vars.browser=='ie'&&skel.vars.IEVersion>9){skel.on('-small !small',function(){$banner.css('height','100vh')});skel.on('+small',function(){$banner.css('height','')})}
+$banner.find('.more').addClass('scrolly')}
+if($(".scrolly").length){$('.scrolly').scrolly()}
+$('#menu').panel({delay:500,hideOnClick:!0,hideOnSwipe:!0,resetScroll:!0,resetForms:!0,side:'right'})})})(jQuery)
